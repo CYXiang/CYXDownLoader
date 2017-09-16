496df2fed7634d485c04b6ece286084d67d5e013
chenyanxiang <chenyanxiang@hoge.cn>
2017-09-16 16:51:49 +0800

    init

---
+++ CYXDownLoader/CYXDownLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYXDownLoader
+//
+//  Created by 陈燕翔 on 2017/9/16.
+//  Copyright © 2017年 陈燕翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYXDownLoader/CYXDownLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYXDownLoader
+//
+//  Created by 陈燕翔 on 2017/9/16.
+//  Copyright © 2017年 陈燕翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
